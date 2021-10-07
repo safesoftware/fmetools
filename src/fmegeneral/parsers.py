88b9b58ad4c84aef6bfad4890611fb210fe38d1e
Carson Lam <carson.lam@safe.com>
2021-10-07 11:32:03 -0700

    parsers: cleanup.

--- src/fmegeneral/parsers.py
+++ src/fmegeneral/parsers.py
@@ -3,30 +3,35 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 from collections import OrderedDict
 from fmeobjects import FMESession
 from fmegeneral import fmeutil
-from fmegeneral.fmeconstants import kFME_TEMPLATE_FEATURE_TYPE
 from fmegeneral.fmeutil import unicodeToSystem, systemToUnicode
 from six import string_types
 
 
 class OpenParameters(OrderedDict):
-    """Interpret the open() parameters given to :meth:`pluginbuilder.FMEReader.open` and
-    :meth:`pluginbuilder.FMEWriter.open`.
+    """
+    Provides convenient access to the open() parameters given to
+    :meth:`pluginbuilder.FMEReader.open` and :meth:`pluginbuilder.FMEWriter.open`.
 
-    Parameter keys that appear multiple times in the parameters list will result in values of type :class:`list`.
-    This scenario is most likely to be encountered with `+ID` keys, though such keys are usually handled
-    by :meth:`pluginbuilder.FMEMappingFile.fetchFeatureTypes` and not within this class.
+    Parameter keys that appear multiple times in the parameters list will result in
+    values of type :class:`list`.
+    This scenario is most likely to be encountered with `+ID` keys,
+    though such keys are usually handled
+    by :meth:`pluginbuilder.FMEMappingFile.fetchFeatureTypes` instead.
 
-    :param str dataset: Dataset value from the first argument to `open()`. Should use `decodedDataset` instead.
-    :param list original: Original parameters passed to `open`.
+    :ivar str dataset: Dataset value from the first argument to `open()`.
+        Should use `decodedDataset` instead.
+    :ivar list original: Original parameters passed to `open`.
     """
 
     def __init__(self, dataset, parameters):
         """
         :type dataset: str or None
-        :param dataset: Dataset value from the first argument to `open()`. Should use `decodedDataset` instead.
-        :param list[str] parameters: open() parameters. Must have an odd number of elements or be empty.
+        :param dataset: Dataset value from the first argument to the
+            reader/writer's `open()`.
+        :param list[str] parameters: `open()` parameters.
+            Must have an odd number of elements or be empty.
         """
-        assert (len(parameters) > 0 and len(parameters) % 2 == 1) or len(parameters) == 0
+        assert (len(parameters) > 0 and len(parameters) % 2 == 1) or not len(parameters)
         super(OpenParameters, self).__init__()
 
         self.__session = FMESession()
@@ -37,12 +42,14 @@ class OpenParameters(OrderedDict):
         self.original = parameters
 
         for i in range(1, len(parameters), 2):
-            key, value = systemToUnicode(parameters[i]), systemToUnicode(parameters[i + 1])
+            key, value = systemToUnicode(parameters[i]), systemToUnicode(
+                parameters[i + 1]
+            )
 
             if self.__contains__(key):
                 # Key already exists in this dictionary.
                 # If the existing value is a list, append this value to it.
-                # Otherwise, turn existing value into a list with existing and new values.
+                # Otherwise, turn existing value into a list and append new value.
                 existingValue = self.__getitem__(key)
                 if isinstance(existingValue, list):
                     existingValue.append(value)
@@ -53,31 +60,30 @@ class OpenParameters(OrderedDict):
 
     @property
     def decodedDataset(self):
-        """Get the WWJD-decoded value of the dataset.
-
-        :rtype: six.text_type
+        """
+        :returns: The FME-decoded value of the dataset.
+        :rtype: str
         """
         return self.__session.decodeFromFMEParsableText(self.dataset)
 
-    def get(self, key, default=None, decodeWWJD=True):
+    def get(self, key, default=None, decode=True):
         """Get an open() parameter.
 
-        :type key: six.text_type
-        :param key: Key to look for.
+        :param str key: Key to look for.
         :param str default: Value to return if key not present.
-        :param bool decodeWWJD: Whether to interpret the value as WWJD-encoded, and return the decoded value.
-        :rtype: six.text_type
+        :param bool decode: Whether to interpret the value as FME encoded,
+            and return the decoded value.
+        :rtype: str
         """
         value = super(OpenParameters, self).get(key, default)
-        if decodeWWJD and isinstance(value, string_types):
+        if decode and isinstance(value, string_types):
             value = self.__session.decodeFromFMEParsableText(value)
         return value
 
-    def getFlag(self, key, default=False):
+    def get_flag(self, key, default=False):
         """Get an open() parameter and interpret its value as a boolean.
 
-        :type key: six.text_type
-        :param key: Key to look for.
+        :param str key: Key to look for.
         :param bool default: Value to return if key not present.
         :rtype: bool
         """
@@ -102,7 +108,7 @@ class SearchEnvelope(object):
     string substitutions.
     """
 
-    def __init__(self, envelope, coordSys=None):
+    def __init__(self, envelope, coordsys=None):
         """
         :param list[list[float]] envelope: [[minX, minY], [maxX, maxY]]
         """
@@ -113,7 +119,7 @@ class SearchEnvelope(object):
         self.minY = envelope[0][1]
         self.maxX = envelope[1][0]
         self.maxY = envelope[1][1]
-        self.coordSys = coordSys
+        self.coordSys = coordsys
 
     def __str__(self):
         return "Envelope<({},{})({},{}) {}>".format(
@@ -122,14 +128,20 @@ class SearchEnvelope(object):
 
 
 def parse_def_line(def_line, option_names):
-    """Iterate through elements in a DEF line and extract elements into easier-
-    to-use structures.
+    """
+    Iterate through elements in a DEF line and extract elements into
+    more convenient structures.
 
     :param list[str] def_line: The DEF line. Must have an even number of elements.
-    :param list[str] option_names: If a key matches one of these names, it'll be separated from the attributes.
-    :return: Tuple of Unicode feature type, ordered dictionary of attributes and their types,
-       and dictionary of options and their values. Values in the options dictionary are WWJD-decoded.
-       All keys and values are in Unicode.
+    :param list[str] option_names: If a key matches one of these names,
+        it'll be separated from the attributes.
+    :return: Tuple of:
+
+        - feature type
+        - ordered dictionary of attributes and their types (values may be FME-encoded)
+        - dictionary of options and their values
+
+       All keys and values are strings.
     :rtype: str, collections.OrderedDict, dict
     """
     assert len(def_line) % 2 == 0
@@ -138,7 +150,9 @@ def parse_def_line(def_line, option_names):
 
     attributes, options = OrderedDict(), {}
     for index in range(2, len(def_line), 2):
-        key, value = systemToUnicode(def_line[index]), systemToUnicode(def_line[index + 1])
+        key, value = systemToUnicode(def_line[index]), systemToUnicode(
+            def_line[index + 1]
+        )
 
         if key in option_names:
             options[key] = session.decodeFromFMEParsableText(value)
@@ -154,11 +168,11 @@ def get_template_feature_type(feature):
     :meth:`fmeobjects.FMEFeature.getFeatureType` otherwise. These are the feature types
     found on DEF lines when FME writers are in dynamic mode.
 
-    :param fmeobjects.FMEFeature feature: Feature to query.
-    :return: Feature type to look for on DEF lines. Converted to Unicode.
-    :rtype: six.text_type
+    :param FMEFeature feature: Feature to query.
+    :return: Feature type to look for on DEF lines.
+    :rtype: str
     """
-    template_feature_type = feature.getAttribute(kFME_TEMPLATE_FEATURE_TYPE)
+    template_feature_type = feature.getAttribute("fme_template_feature_type")
     return systemToUnicode(template_feature_type or feature.getFeatureType())
 
 
@@ -185,7 +199,9 @@ class JSONOptionParser(object):
         self.def_options = def_line_options
         self.inner_params = {}
 
-        source_params = def_line_options.get(self.SOURCE_PARAMS_OPTION_ATTR, "").split(";")
+        source_params = def_line_options.get(self.SOURCE_PARAMS_OPTION_ATTR, "").split(
+            ";"
+        )
         if len(source_params) >= 2:
             session = FMESession()
             for i in range(0, len(source_params), 2):
@@ -198,7 +214,9 @@ class JSONOptionParser(object):
 
         :return: Document source setting value, or 'Feature' if not set.
         """
-        return self.def_options.get(self.SOURCE_OPTION_ATTR, self.DOCUMENT_SOURCE_FEATURE)
+        return self.def_options.get(
+            self.SOURCE_OPTION_ATTR, self.DOCUMENT_SOURCE_FEATURE
+        )
 
     def document_source_is_feature(self):
         """Returns True if the writer should build JSON documents out of the Feature.
