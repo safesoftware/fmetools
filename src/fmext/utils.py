d693eebd0c5c1065dce737316d5eb16427909f6a
Carson Lam <carson.lam@safe.com>
2021-10-12 14:49:16 -0700

    string_to_bool(): refactor.

--- src/fmext/utils.py
+++ src/fmext/utils.py
@@ -27,31 +27,28 @@ def stringarray_to_dict(stringArray):
 
 
 def string_to_bool(string):
-    """Converts a string to boolean, and returns None if conversion fails.
+    """
+    Converts a string to boolean using FME semantics.
+    Intended for parsing boolean values from the FME GUI that were serialized as
+    string literals such as yes/no and true/false.
 
     :param str string: Value to convert.
+    :returns: boolean, or None if value wasn't a parseable to boolean
     :rtype: bool
     """
     #  This method is modeled after STF_stringToBoolean from stfutil2.cpp.
-
-    if len(string) == 0:
+    try:
+        first_char = string[0].lower()
+    except (TypeError, IndexError):
         return None
 
-    first_char = string[0].lower()
-
-    # Check if first character contains "t" or "y".
     if first_char in ["t", "y"]:
         return True
 
-    # Check if first character contains "f" or "n".
     if first_char in ["f", "n"]:
         return False
 
-    # Attempt to convert string to float.
-    else:
-        try:
-            # Returns True if string casts to float as a non-zero number. False if not equal to zero.
-            return bool(float(string))
-        except (TypeError, ValueError):
-            # Conversion failed.
-            return None
+    try:
+        return bool(float(string))
+    except (TypeError, ValueError):
+        return None
