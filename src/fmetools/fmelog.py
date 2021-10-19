a912e480256774f495821ce5e0e6e9b2bf68eac0
Carson Lam <carson.lam@safe.com>
2021-10-19 15:57:44 -0700

    fmelog: minor refactoring and comment/doc formatting.

--- src/fmetools/fmelog.py
+++ src/fmetools/fmelog.py
@@ -1,14 +1,18 @@
 """
-Logging system for FME that builds upon the Python standard library's
-logging module, but emits messages to FMELogFile under the hood, and adds
-functionality.
-
-Internal use only.
+This module bridges FME's :class:`FMELogFile`
+with the :mod:`logging` module in the Python standard library.
 """
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-from fmeobjects import FMELogFile, FMEFeature, FME_WARN, FME_INFORM, FME_ERROR, FME_FATAL
+from fmeobjects import (
+    FMELogFile,
+    FMEFeature,
+    FME_WARN,
+    FME_INFORM,
+    FME_ERROR,
+    FME_FATAL,
+)
 import fme
 
 import logging
@@ -18,7 +22,8 @@ from six import string_types
 class FMELogFormatter(logging.Formatter):
     """Formats log records for display.
 
-    For message numbers, the message and parameters are sent to :class:`fmeobjects.FMELogFile`, and the resolved message string is returned.
+    For message numbers, the message and parameters are sent to
+    :class:`fmeobjects.FMELogFile`, and the resolved message string is returned.
     For string messages, string substitution is performed, and the result returned.
 
     For messages at DEBUG level, a 'DEBUG: ' prefix is added.
@@ -37,12 +42,12 @@ class FMELogFormatter(logging.Formatter):
             except AttributeError:
                 # LogRecords don't necessarily have prepended_params, as it's something we add.
                 pass
-            # FMELogFile is local due to FMESession leak issues when participating in Python's singleton logging system.
+            # FMELogFile is local due to FMESession issues when used in Python's singleton logging system.
             msg_string = FMELogFile().getMessage(
                 record.msg, FMELogFormatter.cast_msg_params(msg_params)
             )
         else:
-            # Only attempt substitutions in string messages if caller provided arguments.
+            # Only attempt substitutions in string messages if caller gave arguments.
             # Otherwise, '%xx' in message could be misinterpreted as placeholders and cause errors.
             msg_string = record.msg
             if record.args:
@@ -86,9 +91,11 @@ class FMELogHandler(logging.Handler):
     """Bridges the world of Python logging with the world of FME logging.
 
     Maps Python log levels to FME message severities.
-    Messages are resolved and forwarded to :class:`fmeobjects.FMELogFile` with the appropriate severity level.
+    Messages are resolved and forwarded to :class:`fmeobjects.FMELogFile`
+    with the appropriate severity level.
 
-    :class:`fmeobjects.FMEFeature` logging is intercepted here and sent straight to :class:`fmeobjects.FMELogFile`.
+    :class:`fmeobjects.FMEFeature` logging is intercepted here and sent straight to
+    :class:`fmeobjects.FMELogFile`.
     """
 
     def __init__(self):
@@ -99,7 +106,7 @@ class FMELogHandler(logging.Handler):
         return 0
 
     def emit(self, record):
-        # FMELogFile is local due to FMESession leak issues when participating in Python's singleton logging system.
+        # FMELogFile is local due to FMESession leak when used in Python's singleton logging system.
         fme_severity = LEVEL_NUM_TO_FME[record.levelno]
         if isinstance(record.msg, FMEFeature):
             FMELogFile().logFeature(record.msg, fme_severity, 20)
@@ -108,7 +115,7 @@ class FMELogHandler(logging.Handler):
         msgStr = self.format(record)
         FMELogFile().logMessageString(msgStr, fme_severity)
 
-    # This ensures that only one instance of this class can be in a Logger's list of handlers.
+    # Ensure that only one instance of this class can be in a Logger's list of handlers.
     def __eq__(self, other):
         return isinstance(other, self.__class__)
 
@@ -121,7 +128,8 @@ class FMELoggerAdapter(logging.LoggerAdapter):
     off any message parameter list. For use with :class:`FMELogFormatter`,
     which recognizes the keyword arguments involved.
 
-    Logging methods accept a keyword ``no_prepend_args``. If True, nothing will be prepended to message parameters.
+    Logging methods accept a keyword ``no_prepend_args``.
+    If True, nothing will be prepended to message parameters.
 
     :ivar prepended_params: Message parameters to prepend to all message parameters.
        Intended for frequent log message prefixes, like format name and direction.
@@ -129,7 +137,7 @@ class FMELoggerAdapter(logging.LoggerAdapter):
 
     def __init__(self, logger, extras):
         super(FMELoggerAdapter, self).__init__(logger, extras)
-        self.prepended_params = None
+        self.prepended_params = []
 
     def process(self, msg, kwargs):
         if "extra" not in kwargs:
@@ -147,7 +155,8 @@ class FMELoggerAdapter(logging.LoggerAdapter):
 
 
 def get_configured_logger(name="fmelog", debug=None):
-    """Get an object that can be used for Python-style logging.
+    """
+    Get an object that can be used for Python-style logging.
 
     :param str name: Logger name to obtain.
     :param bool debug: Whether debug-level messages should be logged.
@@ -164,7 +173,7 @@ def get_configured_logger(name="fmelog", debug=None):
     logger.setLevel(logging.DEBUG if debug else logging.INFO)
     # Don't duplicate messages to the parent (root) logger.
     # The root logger isn't set up to handle FME msgnums,
-    # causing an exception message in stderr every time a msgnum is logged in unit tests.
+    # causing an exception in stderr every time a msgnum is logged in unit tests.
     logger.propagate = False
 
     # Method checks for duplicates before adding.
