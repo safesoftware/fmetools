d2e998e04440f26cb9d82a362ebea71d4080ec1a
Heather Li <heather.li@safe.com>
2021-11-04 11:55:13 -0700

    add string localization helper functions

---
+++ src/fmetools/localize.py
@@ -0,0 +1,31 @@
+"""
+Helpers for enabling localized strings in Python package code via gettext.
+
+"""
+import os
+
+
+def _get_default_locale_dir():
+    """
+    Return the directory where localization files are stored.
+    Assumes that fmetools has been vendorized as a whl according to the fpkg spec.
+    """
+    return os.path.abspath(__file__ + "/../../../i81n")
+
+
+def enable_module_localization(python_module_name, locale_dir=None, enable_fallback=True, **kwargs):
+    """
+    Attempt to load localized messages. Uses the default `gettext` behaviour to determine the language.
+
+    :param str python_module_name: Name of the python module to localize e.g. `fmepy_module`
+    :param str locale_dir: locale dir to look for .mo files in. If not specified, uses :func:`_get_default_locale_dir`
+    :param bool enable_fallback: Whether to use the original localized strings if a .mo file cannot be found
+    :param kwargs: additional keyword arguments to pass to ``gettext.translation()``
+    :return: configured gettext
+    """
+    import gettext
+    if not locale_dir:
+        locale_dir = _get_default_locale_dir()
+    t = gettext.translation(python_module_name, locale_dir, fallback=enable_fallback, **kwargs)
+    tr = t.gettext
+    return tr
