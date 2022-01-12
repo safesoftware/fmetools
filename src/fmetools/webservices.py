11d62ac0d8efd6d533c3cc0869261759af799bde
Heather Li <heather.li@safe.com>
2022-01-12 14:30:08 -0800

    wrap _create_auth_from_named_connection for set_session_auth_from_named_connection and get_named_connection_auth

--- src/fmetools/webservices.py
+++ src/fmetools/webservices.py
@@ -125,6 +125,26 @@ class FMETokenConnectionWrapper(object):
         return self.wrapped_conn.getWebService().supportQueryStringAuthorization()
 
 
+def _create_auth_from_named_connection(conn, client_name=""):
+    """
+    Get a configured authentication object from a Named Connection
+    for use with Requests.
+
+    :param FMENamedConnection conn: The Named Connection / Web Connection object.
+    :param str client_name: Name to use for the log message prefix in the failure case,
+        e.g. format or transformer name.
+    :raises TypeError: If the connection is not a valid Web Connection.
+    :rtype: AuthBase
+    """
+    if isinstance(conn, FMEBasicConnection):
+        auth_type = conn.getAuthenticationMethod()
+        user, pwd = conn.getUserName(), conn.getPassword()
+        return get_auth_object(CONN_AUTH_METHOD_TO_KEYWORD[auth_type], user, pwd, client_name)
+    if isinstance(conn, (FMEOAuthV2Connection, FMETokenConnection)):
+        return FMEWebConnectionTokenBasedAuth(FMETokenConnectionWrapper(conn))
+    raise TypeError(tr("Unexpected connection type {}").format(repr(conn)))
+
+
 def set_session_auth_from_named_connection(session, connection_name, client_name):
     """
     Looks up a configured authentication object from a Named Connection and
@@ -133,16 +153,18 @@ def set_session_auth_from_named_connection(session, connection_name, client_name
     This method handles implementation of the SSL verification setting on Web Connections.
 
     :param requests.Session|FMERequestsSession session: web session to set the auth on
-    :param connection_name: Name of the Named Connection / Web Connection. It's an error if no such connection exists.
-    :param client_name: Name to use for the log message prefix in the failure case, e.g. format or transformer name.
+    :param connection_name: Name of the Named Connection / Web Connection.
+        It's an error if no such connection exists.
+    :param client_name: Name to use for the log message prefix in the failure case,
+        e.g. format or transformer name.
     """
-    auth = get_named_connection_auth(connection_name, client_name)
-
-    # get_named_connection_auth not raising an error implies that the web connection exists
     conn = NamedConnectionManager().getNamedConnection(connection_name)
-    assert isinstance(conn, (FMEBasicConnection, FMEOAuthV2Connection, FMETokenConnection))
+    if not conn:
+        raise NamedConnectionNotFound(client_name, connection_name)
 
-    session.auth = auth
+    # this will raise an error if an auth object cannot be successfully created
+    session.auth = _create_auth_from_named_connection(conn, client_name)
+
     session.verify = conn.getVerifySslCertificate()
 
 
@@ -161,13 +183,8 @@ def get_named_connection_auth(connection_name, client_name):
     conn = NamedConnectionManager().getNamedConnection(connection_name)
     if not conn:
         raise NamedConnectionNotFound(client_name, connection_name)
-    if isinstance(conn, FMEBasicConnection):
-        auth_type = conn.getAuthenticationMethod()
-        user, pwd = conn.getUserName(), conn.getPassword()
-        return get_auth_object(CONN_AUTH_METHOD_TO_KEYWORD[auth_type], user, pwd, client_name)
-    if isinstance(conn, (FMEOAuthV2Connection, FMETokenConnection)):
-        return FMEWebConnectionTokenBasedAuth(FMETokenConnectionWrapper(conn))
-    raise TypeError(tr("Unexpected connection type {}").format(repr(conn)))
+
+    return _create_auth_from_named_connection(conn, client_name)
 
 
 class FMEWebConnectionTokenBasedAuth(AuthBase):
