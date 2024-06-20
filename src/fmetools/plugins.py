b03e76df1d148ffa00d0acb63a39b0368b5c7cf5
Heidi Tong <heidi.tong@safe.com>
2024-06-20 13:06:18 -0700

    Add brackets for PY_OUTPUT_TAGS clause in input_from docstring

--- src/fmetools/plugins.py
+++ src/fmetools/plugins.py
@@ -408,7 +408,7 @@ class FMEBaseTransformer:
                 PY_INPUT_TAGS { INPUT0 INPUT1 }
                 $(INPUT_LINES)
                 SYMBOL_NAME { symbol_name }
-                PY_OUTPUT_TAGS Output <Rejected>
+                PY_OUTPUT_TAGS { Output <Rejected> }
                 OUTPUT { Output FEATURE_TYPE $(OUTPUT_Output_FTYPE)
                     $(OUTPUT_Output_FUNCS) }
                 OUTPUT { <Rejected> FEATURE_TYPE $(OUTPUT_<Rejected>_FTYPE)
